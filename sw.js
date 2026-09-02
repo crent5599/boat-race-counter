ee79fa1751b0d7264903e71697fdc7e26b6cc54d
crent5599 <ika87244@gmail.com>
2026-09-02 19:59:56 +0900

    Delete sw.js

--- sw.js
+++
@@ -1,8 +0,0 @@
-
-const CACHE = "boat-counter-v1";
-const FILES = ["./", "./index.html", "./manifest.webmanifest", "./icon-180.png", "./icon-512.png"];
-self.addEventListener("install", e => e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES))));
-self.addEventListener("activate", e => e.waitUntil(self.clients.claim()));
-self.addEventListener("fetch", e => {
-  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
-});
